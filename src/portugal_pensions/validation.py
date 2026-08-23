5755858b44b47ef320598579386f2acdd35efac2
Diogo Ribeiro <diogo_dj@hotmail.com>
2026-08-23 09:15:57 +0100

    Add manuscript scope section

--- src/portugal_pensions/validation.py
+++ src/portugal_pensions/validation.py
@@ -3496,6 +3496,7 @@ def validate_manuscript_section_boundaries(
         "MS_METHOD",
         "MS_RELATED",
         "MS_QUESTIONS",
+        "MS_SCOPE",
         "MS_ARCHITECTURE",
         "MS_ACCOUNTING",
         "MS_CGA",
